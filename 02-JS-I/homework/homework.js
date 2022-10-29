d001eafc9da336ad1500747f95b3b86eeeb63a9d
Juan Rojas <juanger143@yahoo.com>
2022-10-29 14:23:03 -0300

    primer cambio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,10 +1,12 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
+const { stringify } = require("git-url-parse");
+
 // Crea una variable "string", puede contener lo que quieras:
 const nuevaString = null;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 'hola';
 
 // Crea una variable booleana:
 const nuevoBool = null;
@@ -14,7 +16,6 @@ const nuevaResta = 10 - null === 5;
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * null === 40 ;
-
 // Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === null;
 
